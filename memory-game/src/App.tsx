a62148a4bdf71e5e925e4c54e5c97d1e1bc596c8
Lucas Maieski <lucasgrigol@gmail.com>
2023-04-25 22:03:17 -0300

    criado area de informacoes do jogo e estilizado

--- memory-game/src/App.tsx
+++ memory-game/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png';
+import RestartIcon from './svgs/restart.svg';
+import { Button } from './components/Button';
+import { InfoItem } from './components/InfoItem';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const resetAndCreateGrid = () => {
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    }
+
+    return (
+        <C.Container>
+            <C.Info>
+                <C.LogoLink href="">
+                    <img src={logoImage} width="200" alt="" />
+                </C.LogoLink>
+                <C.InfoArea>
+                    <InfoItem label="Tempo" value="00:00"/>
+                    <InfoItem label="Movimentos" value="0"/>
+                </C.InfoArea>
+                <Button icon={RestartIcon} label="Reiniciar" onClick={resetAndCreateGrid} />
+            </C.Info>
+            <C.GridArea>
+
+            </C.GridArea>
+        </C.Container>
+    )
 }
 
 export default App
